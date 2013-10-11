7a566c91ea116623dd2225dd43f2ee43ba964aa6
Matt Roman <romanmt@gmail.com>
2013-10-11 08:09:24 -0400

    Combines client files into a single file

--- client/epsilon_greedy.js
+++ client/epsilon_greedy.js
@@ -1,6 +1,28 @@
 (function(global) {
   "use strict";
 
+  function Levers() {
+    var exports = {
+      select: function(ids, levers, defaults) {
+        function mapper(element) {
+          return levers[element] || _({name: element}).extend(defaults);
+        }
+        return _(ids).map(mapper);
+      },
+
+      objectify: function(levers) {
+        function reducer(obj, value) {
+          obj[value.name] = value;
+          return obj;
+        }
+        var object = _(levers).reduce(reducer, {})
+        return object;
+      }
+    }
+    return exports;
+  }
+
+  global.Levers = Levers;
   function EpsilonGreedy(spec) {
 
     function selectRandom() {
