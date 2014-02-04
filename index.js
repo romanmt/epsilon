300b46040e790847dbad71d596b3be54a8590741
jperkelens <jperkelens@gmail.com>
2014-02-04 11:36:16 -0500

    Fixes index, groups tests, version bump

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var epsilon = require('./lib/controllers/epsilon_controller')
+module.exports = {
+  middleware: function() {
+    return function (req, res, next) {
+      if(!req.url.match(/^\/epsilon\/epsilon_greedy.js$/)) return next();
+      console.log(__dirname + '/client/epsilon_greedy.js')
+      res.status(200).sendfile("/client/epsilon_greedy.js", {root: __dirname})
+    }
+  },
 
-exports.middleware = function() {
-  return function (req, res, next) {
-    if(!req.url.match(/^\/epsilon\/epsilon_greedy.js$/)) return next();
-    console.log(__dirname + '/client/epsilon_greedy.js')
-    res.status(200).sendfile("/client/epsilon_greedy.js", {root: __dirname})
+  bind: function(app) {
+    app.get('/epsilon/test/:testName', epsilon.get)
+    app.put('/epsilon/test', epsilon.update)
   }
-}
-
-exports.bind = function(app) {
-  app.get('/epsilon/test/:testName', epsilon.get)
-  app.put('/epsilon/test', epsilon.update)
 }
