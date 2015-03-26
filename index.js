3000ce6238ae906e8c4c0e4d4e530f94fd23e7f9
Matt Roman <romanmt@gmail.com>
2015-03-26 12:15:13 -0400

    removed console.log

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
   middleware: function() {
     return function (req, res, next) {
       if(!req.url.match(/^\/epsilon\/epsilon_greedy.js$/)) return next();
-      console.log(__dirname + '/client/epsilon_greedy.js')
       res.status(200).sendfile("/client/epsilon_greedy.js", {root: __dirname})
     }
   },
